c3e2586067da2291b8aab3d8daca6bd8a21aa916
carlos alberto gonzalez <carlos.gonzalez923@gmail.com>
2024-05-25 00:31:38 -0500

    create schema user

---
+++ BackEnd/model/userSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: [3, "First Name Must Contain At Least 3 Characters!"],
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: [3, "Last Name Must Contain At Least 3 Characters!"],
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Please Provide A Valid Email!"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
+    maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
+  },
+  message: {
+    type: String,
+    required: true,
+    minLength: [13, "NIC Must Contain Exact 13 Digits!"],
+    maxLength: [13, "NIC Must Contain Exact 13 Digits!"],
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
