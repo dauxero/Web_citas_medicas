5c2a7b8a76115992d357daeb1a0e035636367125
carlos alberto gonzalez <carlos.gonzalez923@gmail.com>
2024-05-26 23:59:17 -0500

    funciones de token con caducidad en el esquema de usuario, comparacion de password y hasheo de encriptado de password

--- BackEnd/model/userSchema.js
+++ BackEnd/model/userSchema.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonWebToken";
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -26,9 +28,57 @@ const userSchema = new mongoose.Schema({
   message: {
     type: String,
     required: true,
-    minLength: [13, "NIC Must Contain Exact 13 Digits!"],
-    maxLength: [13, "NIC Must Contain Exact 13 Digits!"],
+    minLength: [10, "NIC Must Contain Exact 13 Digits!"],
+    maxLength: [10, "NIC Must Contain Exact 13 Digits!"],
+  },
+  dbo: {
+    type: Date,
+    required: [true, "DOB is required"],
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female"],
+  },
+  password: {
+    type: String,
+    minLength: [11, "Password Must Contain At Least 8 characters!"],
+    required: true,
+    select: false,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["Admin", "Patient", "Doctor"],
+  },
+  doctorDeparment: {
+    type: String,
+  },
+  docAvatar: {
+    public_id: String,
+    url: String,
   },
 });
+
+//?funcion de modificacion de password / encriptado
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//?metodo de comparacion de password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//generador y caducidad del token
+userSchema.methods.generateJsonWebToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
 
 export const User = mongoose.model("User", userSchema);
