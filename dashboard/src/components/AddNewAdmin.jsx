3d35ee7fb69251f8d3eb9579301181bd76b5d058
carlos gonzalez <carlos.gonzalez923@gmail.com>
2024-06-25 20:57:10 -0500

    Creacion de sidebar y routing de app

---
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddNewAdmin = () => {
+  return (
+    <>
+      <h1>New Admin</h1>
+    </>
+  );
+};
+
+export default AddNewAdmin;
---
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddNewDoctor = () => {
+  return (
+    <>
+      <h1>doctor</h1>
+    </>
+  );
+};
+
+export default AddNewDoctor;
